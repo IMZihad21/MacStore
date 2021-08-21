e7eb4989924e13d389207836aa3b42dfa7ebf181
ZèD <imzihad@gmail.com>
2021-08-21 11:23:01 +0600

    Setup Variables
    Signed-off-by: ZèD <imzihad@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* ------------------
+    Init Variables
+------------------- */
+// Memory
+const memory8GB = document.getElementById('memory8GB');
+const memory16GB = document.getElementById('memory16GB');
+
+// Storage
+const storage256GB = document.getElementById('storage256GB');
+const storage512GB = document.getElementById('storage512GB');
+const storage1TB = document.getElementById('storage1TB');
+
+// Delivery Cost
+const deliveryFree = document.getElementById('deliveryFree');
+const delivery20 = document.getElementById('delivery20');
+
+// Prices
+let bestPrice = document.getElementById('bestPrice');
+let memoryCost = document.getElementById('memoryCost');
+let storageCost = document.getElementById('storageCost');
+let deliveryCharge = document.getElementById('deliveryCharge');
+let totalPrice = document.getElementById('totalPrice');
+
+// Promo
+const promoApply = document.getElementById('promoApply');
+let promoCode = document.getElementById('promoCode');
+let promoTotal = document.getElementById('promoTotal');
+
